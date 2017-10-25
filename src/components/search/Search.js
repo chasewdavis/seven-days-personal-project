aad8507d1d3e22b6f61803843350a012b4e17959
Chase Davis <chasewdavis7@gmail.com>
2017-10-24 23:03:48 -0600

    Improved functionality in search component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,6 +5,10 @@ import search from '../../svg/search.svg';
 import './search.css';
 import right from '../../svg/send2.svg';
 
+let challengeButton = <button></button>;
+let users = <div></div>;
+let prompts = [];
+
 export default class Search extends Component {
 
     constructor(props){
@@ -15,7 +19,8 @@ export default class Search extends Component {
             searchInput: '',
             foundUsers: [],
             sentChallenges: [], // challenges that user has already sent so they cant send twice, displays properly etc.
-            matches: [] // goals that you've sent that match with the users that are being displayed
+            matches: [], // goals that you've sent that match with the users that are being displayed
+            justSent: [] //prevents user from sending invite 2 or more times to a single user 
         }
     }
 
@@ -27,6 +32,8 @@ export default class Search extends Component {
             sentChallenges: [],
             matches: []
         })
+        users = <div></div>;
+        prompts = [];
     }
 
     componentDidMount(){
@@ -34,7 +41,11 @@ export default class Search extends Component {
             this.setState({
                 goalname: res.data[0].goalname,
             })
-        }); 
+        });
+        axios.get(`/api/getSetChallenges/${this.props.match.params.id}`)
+        .then(challenges => {
+            this.setState({sentChallenges:challenges.data})
+        })
     }
 
     inputHandler(val){
@@ -48,91 +59,71 @@ export default class Search extends Component {
             axios.get(`/api/findFriends/${this.props.match.params.id}/${this.state.searchInput}`)
             .then(friends=>{
                 console.log('the friends you are searching for...', friends.data);
-                this.setState({foundUsers:friends.data});
-                // see if this challenge has already been sent / accepted 
-                axios.get(`/api/getSetChallenges/${this.props.match.params.id}`)
-                .then(challenges => {
-                    console.log('the challenges that you\'ve sent...', challenges.data)
-                    this.setState({sentChallenges:challenges.data},
-                    () => {
-                        //look for matches
-                        let temp = this.state.foundUsers.map(e=>e.id)
-                        console.log('the temp is..', temp)
-                        let matches = this.state.sentChallenges.filter(e=> {
-                            if(temp.indexOf(e.userid)!==-1){
-                            return e
-                            }
-                        })
-                        console.log('the matches are...', matches)
 
-                        this.setState({matches: matches })  
+                let mappedFriends = friends.data.map(e=>e.id)
+                let mappedChallenges = this.state.sentChallenges.map(e=>e.userid)
+                let matchesFound = mappedFriends.filter(e=>mappedChallenges.indexOf(e)!==-1?e:null)
+                
+                users = friends.data.map((e,i)=>{
+
+                    if(matchesFound.indexOf(e.id)!==-1){
+                        prompts.push('Challenge Sent')
+                        return (
+                            <div className='users_found' key={i}>
+                                <div className='user_img'>
+                                    <img src={e.img} />
+                                </div>
+                                <div className='user_name'>
+                                    {e.user_name}
+                                </div>
+                                <button>Challenge Sent</button>
+                            </div> 
+                        )
+                    }else{
+                        prompts.push('Challenge')
+                        return (
+                            <div className='users_found' key={i}>
+                                <div className='user_img'>
+                                    <img src={e.img} />
+                                </div>
+                                <div className='user_name'>
+                                    {e.user_name}
+                                </div>
+                                <button onClick={()=>{this.challengeFriend(e.id);this.changePrompt(i)}}>{prompts[i]}</button>
+                            </div>
+                        )
+                    }
                 })
+
+                this.setState({foundUsers:friends.data, matches:matchesFound})
             })
-        })
         }else{
             console.log('Input field is blank')
         }
     }
 
     challengeFriend(user){
-        console.log(user)
-        axios.post(`/api/challengeFriend/${this.props.match.params.id}`, { friend: user })
-        .then(res=>console.log(res))
-    }
 
-    challengeButtonPrompt(val,index){
+        if(this.state.justSent.indexOf(user)===-1){
+            let temp = this.state.justSent;
+            temp.push(user)
+            this.setState({justSent:temp})
 
-        if(this.state.matches[index]){
-            console.log('matches that challenge button Propmt seeees', this.state.matches)
-            console.log('what I\'m comparing', this.state.matches[index].userid, val.id)
-            if(this.state.matches[index].userid === val.id){
-                console.log('MATCH FOUND')
-                return (
-                    <div>
-                        Challenge Sent
-                    </div>
-                )
-            }else{
-                return (
-                    <div>
-                        Challenge<img src={right} alt='right arrow'/>
-                    </div>
-                )
-            }
-        }else{
-            return (
-                <div>
-                    Challenge<img src={right} alt='right arrow'/>
-                </div>
-            )
+            console.log(user)
+            axios.post(`/api/challengeFriend/${this.props.match.params.id}`, { friend: user })
+            .then(res=>{
+                this.findFriends();
+                console.log('friend has been challenged', res) 
+            })
         }
     }
 
+    changePrompt(index){
+        prompts[index] = 'Challenge Sent';
+        this.forceUpdate();
+    }
+
     render(){
-
-        let users = this.state.foundUsers.map((e,i) => {
-            // console.log(e)
-            return ( 
-                <div className='users_found' key={i}>
-                    <div className='user_img'>
-                        <img src={e.img} />
-                    </div>
-                        {/* {e.user_name} */}
-                    <div className='user_name'>
-                        {e.user_name}
-                    </div>
-                    <button onClick={()=>this.challengeFriend(e.id)}>
-
-                        {/* should change based on whether or not a match is found */}
-                        {this.challengeButtonPrompt(e,i)}
-                        {/* Challenge<img src={right} alt='right arrow'/>  */}
-
-                    </button>
-                </div> 
-            )
-        })
-
-        console.log(this.state)
 
         return (
             <div>
